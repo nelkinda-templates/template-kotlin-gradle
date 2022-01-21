eedce9e42eb9d72aaa2fe58274a4a03e155caccd
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-01-21 21:02:06 +0530

    Update Cucumber to JUnit Suites.

--- src/test/kotlin/com/nelkinda/template/acceptance/AcceptanceTest.kt
+++ src/test/kotlin/com/nelkinda/template/acceptance/AcceptanceTest.kt
@@ -1,13 +1,19 @@
 package com.nelkinda.template.acceptance // TODO change
 
-import io.cucumber.junit.CucumberOptions
-import io.cucumber.junit.platform.engine.Cucumber
-import org.junit.runner.RunWith
+import io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME
+import org.junit.platform.suite.api.ConfigurationParameter
+import org.junit.platform.suite.api.ConfigurationParameters
+import org.junit.platform.suite.api.IncludeEngines
+import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.Suite
 
-@CucumberOptions(
-    features = ["src/test/resources/features"],
-    tags = "not(@wip) and not(@future)",
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameters(
+    ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "not(@wip) and not(@future)"
+    )
 )
-@RunWith(io.cucumber.junit.Cucumber::class)
-@Cucumber
 class AcceptanceTest
