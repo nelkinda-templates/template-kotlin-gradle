1796b81584543bb0d135282e53d78085b8214562
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-08-12 12:27:03 +0530

    Bump io.cucumber dependency set from 7.2.3 to 7.6.0 and change the acceptance tests accordingly.

--- src/test/kotlin/com/nelkinda/template/acceptance/AcceptanceTest.kt
+++ src/test/kotlin/com/nelkinda/template/acceptance/AcceptanceTest.kt
@@ -1,8 +1,5 @@
 package com.nelkinda.template.acceptance // TODO change
 
-import io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME
-import org.junit.platform.suite.api.ConfigurationParameter
-import org.junit.platform.suite.api.ConfigurationParameters
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
@@ -10,10 +7,4 @@ import org.junit.platform.suite.api.Suite
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameters(
-    ConfigurationParameter(
-        key = FILTER_TAGS_PROPERTY_NAME,
-        value = "not(@wip) and not(@future)"
-    )
-)
 class AcceptanceTest
